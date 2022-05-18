433db96503326cef8640c37f609865c7d66255ae
Infinitusvoid <84730862+Infinitusvoid@users.noreply.github.com>
2022-05-19 00:33:53 +0200

    Task

--- Vulkan_Learning/Ring_Example_000.h
+++ Vulkan_Learning/Ring_Example_000.h
@@ -1,5 +1,11 @@
 #pragma once
+
+//#include "Procedural_Ring.h"
+
 class Ring_Example_000
 {
+	//TODO here we will have class that sets the parameter of class Procedural ring and implements methods, and as a result create a cool visual!
+	
+		
 };
 
