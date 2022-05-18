c0d5a81419b86f48908d92cf8bc7d534db880a95
Infinitusvoid <84730862+Infinitusvoid@users.noreply.github.com>
2022-05-18 19:25:05 +0200

    refactoring

--- Vulkan_Learning/Procedural_Ring.h
+++ Vulkan_Learning/Procedural_Ring.h
@@ -5,78 +5,66 @@
 
 struct Procedural_Ring
 {
-		int num_radial_slices;
-		int num_length_slices;
-		std::function<void()> f_init;
-		std::function<void(int, int)> f_update;
-		std::function<float(int, int)> f_radius_External;
-		std::function<float(int, int)> f_radius_Internal;
-		std::function<float(float)> f_disp_l = Procedural_Ring::identitiy;
-		std::function<float(float)> f_disp_r = Procedural_Ring::identitiy;
+	int num_radial_slices;
+	int num_length_slices;
+	std::function<void()> f_init;
+	std::function<void(int, int)> f_update;
+	std::function<float(int, int)> f_radius_External;
+	std::function<float(int, int)> f_radius_Internal;
+	std::function<float(float)> f_disp_l = Procedural_Ring::identitiy;
+	std::function<float(float)> f_disp_r = Procedural_Ring::identitiy;
 
-		static float radius_External_Example(int r, int l)
+	static float const identitiy(const float v) { return v; }
+
+	void init_example_0()
 	{
-		float r_angle = (r / (float)500) * glm::two_pi<float>();
-		float l_fak = (l / (float)500);
-
-		float this_offst = 1.5f +
-			0.25f * glm::sin(r_angle * 10) +
-			0.1f * glm::sin(l_fak * glm::two_pi<float>() * 20.0f);
-		//0.025f * glm::sin(l_fak * glm::two_pi<float>() * 105.0f);
-
-		float lerp_fak = 1 - (abs(l_fak - 0.5f) * 2);
-
-		lerp_fak = pow(lerp_fak, 0.8f);
-		//return std::lerp(Radius_Internal(r, l), this_offst, lerp_fak);
-		// 
-		return std::lerp(0.5f, this_offst, lerp_fak);
-		//return this_offst;
-		//return std::lerp(0.5f, this_offst, l_fak);
-		return  1.0 + l_fak;//l_fak * 2.5f;
+		num_radial_slices = 500;
+		num_length_slices = 500;
+		f_init = []() {};
+		f_update = [](int l, int r) {};
+		f_radius_External = Procedural_Ring::m_example_0_external;
+		f_radius_Internal = Procedural_Ring::m_example_0_internal;
+		f_disp_l = [](float l) { return l * 5.0f; };
+		f_disp_r = Procedural_Ring::identitiy;
 	}
 
-		static float radius_Internal_Example(int r, int l)
+	void build(
+		const std::function<void(glm::vec3&, glm::vec3&, glm::vec3&, glm::vec3&)> f_addQuad
+	)
 	{
-		float r_angle = (r / (float)500) * glm::two_pi<float>();
-		float l_fak = (l / (float)500);
+		float length_slice = 1.0f / (float)num_length_slices;
+		float length_slice_radial = 1.0f / (float)num_radial_slices;
+		f_init(); //TODO use init to calculate staff 
 
-		float this_offst = 0.7f +
-			0.025 * glm::sin(r_angle * 25) +
-			0.025 * glm::sin(l_fak * glm::two_pi<float>() * 8);
-
-
-		float lerp_fak = 1 - (abs(l_fak - 0.5f) * 2);
-
-		lerp_fak = pow(lerp_fak, 0.4f);
-
-		float val = std::lerp(0.5f, this_offst, lerp_fak);
-
-		val = std::min(radius_External_Example(r, l), val);
-
-		//return this_offst;
-		//return std::lerp(0.5f, this_offst, l_fak);
-		return val;
-	}
-	
-		static float const identitiy(const float v)
-	{
-		return v;
-	}
-		
-		void init_example_0()
+		for (int l_index = 0; l_index < num_length_slices; l_index++)
 		{
-			num_radial_slices = 500;
-			num_length_slices = 500;
-			f_init = []() {};
-			f_update = [](int l, int r) {};
-			f_radius_External = Procedural_Ring::radius_External_Example;
-			f_radius_Internal = Procedural_Ring::radius_Internal_Example;
-			f_disp_l = [](float l) { return l * 5.0f; };
-			f_disp_r = Procedural_Ring::identitiy;
+			for (int r_index = 0; r_index < num_radial_slices; r_index++)
+			{
+				//TODO d1 should become d0 in next iteration of loop to avoid unecessery calulations
+
+				float d0 = f_disp_l(l_index * length_slice);
+				float d1 = f_disp_l((l_index + 1) * length_slice);
+
+				float t0 = glm::two_pi<float>() * f_disp_r(length_slice_radial * r_index);
+				float t1 = glm::two_pi<float>() * f_disp_r(length_slice_radial * (r_index + 1));
+
+				float x0 = glm::sin(t0);
+				float y0 = glm::cos(t0);
+
+				float x1 = glm::sin(t1);
+				float y1 = glm::cos(t1);
+
+				f_update(l_index, r_index); //TODO udate to calculate above staff
+
+				m_draw(r_index, l_index, x0, y0, x1, y1, d0, d1, f_addQuad, f_radius_External);
+				m_draw(r_index, l_index, x0, y0, x1, y1, d0, d1, f_addQuad, f_radius_Internal);
+			}
 		}
 
+	}
 
-		void draw(
+	private:
+		static void m_draw(
 			const int r_index,
 			const int l_index,
 			const float x0,
@@ -87,9 +75,9 @@ struct Procedural_Ring
 			const float d1,
 			std::function<void(glm::vec3&, glm::vec3&, glm::vec3&, glm::vec3&)> f_addQuad,
 			std::function<float(int, int)> f_radius
-			)
+		)
 		{
-			
+
 			const float offset_0_0 = f_radius(r_index, l_index);//Radius_External(r_index, l_index);
 			const float offset_0_1 = f_radius(r_index, l_index + 1);
 			const float offset_1_0 = f_radius(r_index + 1, l_index);
@@ -115,122 +103,41 @@ struct Procedural_Ring
 			f_addQuad(v0, v1, v2, v3);
 		}
 
-		void build (
-			const std::function<void(glm::vec3&, glm::vec3&, glm::vec3&, glm::vec3&)> f_addQuad
-		)
+		static float m_example_0_external(int r, int l)
 		{
-			float length_slice = 1.0f / (float)num_length_slices;
-			float length_slice_radial = 1.0f / (float)num_radial_slices;
-			f_init(); //TODO use init to calculate staff 
+			float r_angle = (r / (float)500) * glm::two_pi<float>();
+			float l_fak = (l / (float)500);
 
-		for (int l_index = 0; l_index < num_length_slices; l_index++)
-		{
-			for (int r_index = 0; r_index < num_radial_slices; r_index++)
-			{
-				//TODO d1 should become d0 in next iteration of loop to avoid unecessery calulations
-				//std::cout << "-----" << std::endl;
-				float d0 = f_disp_l(l_index * length_slice);
-				float d1 = f_disp_l((l_index + 1) * length_slice);
+			float this_offst = 1.5f +
+				0.25f * glm::sin(r_angle * 10) +
+				0.1f * glm::sin(l_fak * glm::two_pi<float>() * 20.0f);
+				0.0025f * glm::sin(l_fak * glm::two_pi<float>() * 105.0f);
 
-				float t0 = glm::two_pi<float>() * f_disp_r(length_slice_radial * r_index);
-				float t1 = glm::two_pi<float>() * f_disp_r(length_slice_radial * (r_index + 1));
+			float lerp_fak = 1 - (abs(l_fak - 0.5f) * 2);
+			lerp_fak = pow(lerp_fak, 0.8f);
 
-				float x0 = glm::sin(t0);
-				float y0 = glm::cos(t0);
-
-				float x1 = glm::sin(t1);
-				float y1 = glm::cos(t1);
-
-				f_update(l_index, r_index); //TODO udate to calculate above staff
-
-
-				//std::cout << "pre1 A x1 : " << x1 << std::endl;
-				//std::cout << "pre1 A y1 : " << y1 << std::endl;
-
-				// External ring
-				//{
-					//std::cout << "pre2 A x1 : " << x1 << std::endl;
-					//std::cout << "pre2 A y1 : " << y1 << std::endl;
-					draw(r_index, l_index, x0, y0, x1, y1, d0, d1, f_addQuad, f_radius_External);
-					draw(r_index, l_index, x0, y0, x1, y1, d0, d1, f_addQuad, f_radius_Internal);
-				//}
-				
-				/*
-				{
-					const float offset_0_0 = f_radius_External(r_index, l_index);//Radius_External(r_index, l_index);
-					const float offset_0_1 = f_radius_External(r_index, l_index + 1);
-					const float offset_1_0 = f_radius_External(r_index + 1, l_index);
-					const float offset_1_1 = f_radius_External(r_index + 1, l_index + 1);
-
-					const float x0_d0 = x0 * offset_0_0;
-					const float x0_d1 = x0 * offset_0_1;
-
-					const float y0_d0 = y0 * offset_0_0;
-					const float y0_d1 = y0 * offset_0_1;
-
-					const float x1_d0 = x1 * offset_1_0;
-					const float x1_d1 = x1 * offset_1_1;
-
-					const float y1_d0 = y1 * offset_1_0;
-					const float y1_d1 = y1 * offset_1_1;
-
-
-					glm::vec3 v0(x0_d0, y0_d0, d0);
-					glm::vec3 v1(x0_d1, y0_d1, d1);
-					glm::vec3 v2(x1_d1, y1_d1, d1);
-					glm::vec3 v3(x1_d0, y1_d0, d0);
-
-					
-					f_addQuad(v0, v1, v2, v3);
-
-					//std::cout << "B x1 : " << x1 << std::endl;
-					//std::cout << "B y1 : " << y1 << std::endl;
-
-					//std::cout << "B v2 : " << v2.x << std::endl;
-					//std::cout << "B v2 : " << v2.y << std::endl;
-				}
-				*/
-				
-				
-				// Internal ring
-				/*
-				{
-					const float offset_0_0 = f_radius_Internal(r_index, l_index);
-					const float offset_0_1 = f_radius_Internal(r_index, l_index + 1);
-					const float offset_1_0 = f_radius_Internal(r_index + 1, l_index);
-					const float offset_1_1 = f_radius_Internal(r_index + 1, l_index + 1);
-
-					const float x0_d0 = x0 * offset_0_0;
-					const float x0_d1 = x0 * offset_0_1;
-
-					const float y0_d0 = y0 * offset_0_0;
-					const float y0_d1 = y0 * offset_0_1;
-
-					const float x1_d0 = x1 * offset_1_0;
-					const float x1_d1 = x1 * offset_1_1;
-
-					const float y1_d0 = y1 * offset_1_0;
-					const float y1_d1 = y1 * offset_1_1;
-
-					glm::vec3 v0(x0_d0, y0_d0, d0);
-					glm::vec3 v1(x0_d1, y0_d1, d1);
-					glm::vec3 v2(x1_d1, y1_d1, d1);
-					glm::vec3 v3(x1_d0, y1_d0, d0);
-					f_addQuad(v0, v1, v2, v3);
-
-					
-				}
-				*/
-
-
-			}
+			return std::lerp(0.5f, this_offst, lerp_fak);
 		}
 
-	}
+		static float m_example_0_internal(int r, int l)
+		{
+			float r_angle = (r / (float)500) * glm::two_pi<float>();
+			float l_fak = (l / (float)500);
 
-		
-			
-	
+			float this_offst = 0.7f +
+				0.025 * glm::sin(r_angle * 25) +
+				0.025 * glm::sin(l_fak * glm::two_pi<float>() * 8);
+
+			float lerp_fak = 1 - (abs(l_fak - 0.5f) * 2);
+			lerp_fak = pow(lerp_fak, 0.4f);
+
+			float val = std::lerp(0.5f, this_offst, lerp_fak);
+			return std::min(m_example_0_external(r, l), val);
+		}
+
+
+
+
 };
 
 
